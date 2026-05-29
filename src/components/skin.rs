f093c4c49ae844e9934a383c4fc95b7bf21c57c3
Spheniscine <spheniscine@gmail.com>
2026-05-29 13:47:58 +0800

    try packing emoji as SVG instead

--- src/components/skin.rs
+++ src/components/skin.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 
-use crate::{components::SkinTrait, game::{Card, Skin, SuitSkin}};
+use crate::{components::SkinTrait, game::{Card, Skin, Suit, SuitSkin}};
 
 
 impl SkinTrait<Card> for Skin {
@@ -18,12 +18,28 @@ impl SkinTrait<Card> for Skin {
     }
 
     fn render_suit(&self, card: &Card) -> Element {
-        rsx! {
-            span {
-                font_family: self.suits.font(),
-                position: if self.suits == SuitSkin::Shapes {"relative"},
-                top: if self.suits == SuitSkin::Shapes {"0.12em"},
-                {self.suits.suit_symbol(card.suit)}
+        if self.suits == SuitSkin::Animals {
+            let asset = match card.suit {
+                Suit::Clubs => asset!("assets/emoji/emoji_u1f430.svg"),
+                Suit::Diamonds => asset!("assets/emoji/emoji_u1f981.svg"),
+                Suit::Hearts => asset!("assets/emoji/emoji_u1f98a.svg"),
+                Suit::Spades => asset!("assets/emoji/emoji_u1f427.svg"),
+            };
+            rsx! {
+                img {
+                    style: "height: 1.15em;",
+                    src: asset,
+                    draggable: false,
+                }
+            }
+        } else {
+            rsx! {
+                span {
+                    font_family: self.suits.font(),
+                    position: if self.suits == SuitSkin::Shapes {"relative"},
+                    top: if self.suits == SuitSkin::Shapes {"0.12em"},
+                    {self.suits.suit_symbol(card.suit)}
+                }
             }
         }
     }
