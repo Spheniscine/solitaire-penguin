6b62958b0b936da2762f7fcdf211bfd321237468
Spheniscine <spheniscine@gmail.com>
2026-06-02 14:04:19 +0800

    add music credit to comments

--- src/components/videos.rs
+++ src/components/videos.rs
@@ -1,4 +1,7 @@
 use dioxus::prelude::*;
 
+/// music credit: "Winter" by Roa
 pub const VIDEO_ORIGINAL: Asset = asset!("/assets/videos/gameplay-original.mp4");
+
+/// music credit: "Esperanza" by Roa
 pub const VIDEO_TUXEDO: Asset = asset!("/assets/videos/gameplay-tuxedo.mp4");
