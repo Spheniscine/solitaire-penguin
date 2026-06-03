19c89addc753f602a1133987afe91eb9f63e71b7
Spheniscine <spheniscine@gmail.com>
2026-06-03 14:08:17 +0800

    add shell script to only re-compress new files

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+cp -r ./docs ./docs-old
 rm -r ./docs
 dx bundle --release
 mv ./target/dx/solitaire-penguin/release/web/public ./docs
+sh ./compare_and_compress.sh ./docs-old/assets ./docs/assets
+rm -r ./docs-old
