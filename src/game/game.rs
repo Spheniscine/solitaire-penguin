c38e2413bf3f7e8d0a3e484cefd1977977d00b02
Spheniscine <spheniscine@gmail.com>
2026-06-04 19:37:10 +0800

    remove unneeded guard code

--- src/game/game.rs
+++ src/game/game.rs
@@ -382,8 +382,7 @@ impl GameState {
     pub fn ondoubleclick(&mut self, pos: BoardPos) {
         if self.is_busy() { return; }
         if !self.can_select(pos) { return; } // needed, or illegal stacks can still be moved this way!
-        let depot = pos.depot_index;
-        if DepotIndex(depot).role() == DepotRole::Foundation { return; }
+        
         for dest in [FOUNDATIONS, TABLEAU_COLUMNS, FREECELLS].into_iter().flatten() {
             let dest = BoardPos { depot_index: dest, card_index: self.board.depots[dest].len()};
             if self.can_move(pos, dest) {
